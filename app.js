633ad7daf214f2ba77c50bdcf4e2035abd704332
ry3879 <rachel.y.3879@gmail.com>
2020-06-06 22:35:54 -0500

    Initial Log In Page, Fix Home Page

--- app.js
+++ app.js
@@ -8,51 +8,18 @@ app.get('/', function(req, res) {
   //__dirname is a keyword for the folder your project is located in
 });
 
+app.get('/login', function(req, res) {
+  res.sendFile(path.join(__dirname + '/log-in-page.html'));
+});
+
+app.get('/signup', function(req, res) {
+  res.sendFile(path.join(__dirname + '/sign-up-page.html'));
+});
+
+//All image files will go under public/images
+app.use(express.static('public'));
+
 //set the port to 3000
 var port = "3000";
 app.set('port', port);
 module.exports = app;
-
-/*because I do not understand this code*/
-/*
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;*/
