9dd6f57a9f3c16e60da4625fa89def70e983fe67
ry3879 <rachel.y.3879@gmail.com>
2020-07-04 02:29:15 -0500

    display request + cancel request

--- routes/learn.js
+++ routes/learn.js
@@ -11,7 +11,23 @@ const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 const flash = require('connect-flash');
 
 router.get('/', ensureAuthenticated, function(req,res){
-    res.render(path.resolve(__dirname, "../learn-page.html"));
+    var request = new Request("SELECT * FROM dbo.learn_requests_table WHERE username = @user", function(err){
+        if(err)
+            console.log(err);
+    });
+    request.addParameter("user", TYPES.VarChar, req.user.username);
+    requestArray = [];
+    request.on("row", function(columns){
+        var rowObject ={};
+        columns.forEach(function(column) {
+            rowObject[column.metadata.colName] = column.value;
+        });
+        requestArray.push(rowObject);
+    });
+    request.on("requestCompleted", function(){
+        res.render(path.resolve(__dirname, "../learn-page.html"), {requests:requestArray});
+    });
+    connection.execSql(request);
 });
 
 router.post('/request', ensureAuthenticated, function(req,res){
@@ -53,6 +69,21 @@ router.post('/request', ensureAuthenticated, function(req,res){
         //note if there are multiple users, then we want to do multiple requests, and we should add them onto the flash
         //Also we can include multiple queries in one thing.
         res.send({success:"You sent your requests."});
+    });
+    connection.execSql(request);
+});
+router.post("/cancel", function(req,res){
+    var ID = req.body.ID;
+    var queryRequest = "DELETE FROM dbo.learn_requests_table WHERE dbo.learn_requests_table.ID = @ID; "+
+    "DELETE FROM dbo.accepted_requests_table WHERE dbo.accepted_requests_table.ID = @ID; "+
+    "DELETE FROM dbo.rejected_requests_table WHERE dbo.rejected_requests_table.ID = @ID;";
+    var request = new Request(queryRequest, function(err){
+        if(err)
+            console.log(err);
+    });
+    request.addParameter("ID", TYPES.UniqueIdentifier, ID);
+    request.on("requestCompleted", function(){
+        res.send({success:"success"});
     });
     connection.execSql(request);
 });
